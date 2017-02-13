5ca08b198d7f857d1ca05559b4ccbe32ad845102
Thomas DRS <toderesa97@gmail.com>
2017-02-13 12:06:04 +0000

    Add files via upload

---
+++ db.sql
@@ -0,0 +1,7 @@
+create table my_users(
+    passwd varchar(20) not null,
+    age int(3) not null,
+    city varchar(30) not null,
+    username varchar(25) not null PRIMARY key,
+    name varchar(20) not null
+);
